5db812da1bbc45572d4ca4d3e847a7f8f2bb35dd
KevinRichardDev <kevin.richard0@orange.fr>
2024-01-21 08:01:11 +0100

    Fin de la correction du Projet

--- script.js
+++ script.js
@@ -37,6 +37,8 @@
 
 // CORRECTION
 
+let playOnce = true;
+
 window.addEventListener("scroll", () => {
   // Navbar effect
   if(window.scrollY > 50){
@@ -54,8 +56,15 @@ window.addEventListener("scroll", () => {
   }
 
   //Popup
-  if(scrollValue > 0.85) {
+  if(scrollValue > 0.85 && playOnce) {
     popup.style.opacity = 1;
-    popup.style.transform = "none"
+    popup.style.transform = "none";
+    playOnce = false;
   }
+});
+
+closeBtn.addEventListener("click", () => {
+  popup.style.opacity = 0;
+  popup.style.transform = "translateX(500px)"
 })
+
